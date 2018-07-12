3c074e361096d875a1756b44287ccffafc96348c
Sharon Rolel <sharon.rolel@activeallocator.com>
2018-07-12 16:53:48 -0400

    disable wsocket

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { IStore, getStore, replaceStore } from "./stores/domain-state"
 import App from "./components/app"
 import syncStoreWithBackend from "./stores/utils/socket"
 
-const socket = new WebSocket("ws://localhost:4001")
+// const socket = new WebSocket("ws://localhost:4001")
 
 // To support HMR of store, this ref holds the latest loaded store.
 const storeInstance = observable.box<IStore | null>(null);
@@ -31,7 +31,7 @@ function prepareStore(getStoreFn = getStore) {
     }
     storeInstance.set(newStore);
     (window as any)['store'] = newStore;
-    syncStoreWithBackend(socket, newStore);
+    // syncStoreWithBackend(socket, newStore);
 }
 
 /**
@@ -44,10 +44,10 @@ if (module.hot) {
         prepareStore(require('./stores/domain-state').getStore);
         render();
     });
-    module.hot.accept("./stores/utils/socket", function () {
-        // new socket sync implementation
-        require("./stores/utils/socket").default(socket, storeInstance.get());
-    });
+    // module.hot.accept("./stores/utils/socket", function () {
+    //     // new socket sync implementation
+    //     require("./stores/utils/socket").default(socket, storeInstance.get());
+    // });
     module.hot.accept('./components/app', function () {
         render();
     });
--- webpack/webpack-dev-server.js
+++ webpack/webpack-dev-server.js
@@ -48,28 +48,4 @@ app.listen(port, 'localhost', err => {
   opn(`http://localhost:${port}`);
 });
 
-var WebSocketServer = require("ws").Server;
-var wss = new WebSocketServer({ port: 4001 });
-
-var connections = {};
-
-wss.on("connection", function connection(ws) {
-  var uuid = "" + Math.random()
-  connections[uuid] = ws;
-  ws.on("message", function incoming(message) {
-    message.sender = ws.uuid;
-    broadcast(uuid, message);
-  });
-});
-
-function broadcast(sender, message) {
-  // console.log("\n" + message)
-  for (var key in connections)
-    if (key !== sender) {
-      try {
-        connections[key].send(message);
-      } catch (e) {
-        delete connections[key];
-      }
-    }
-}
+require('./websocket');
